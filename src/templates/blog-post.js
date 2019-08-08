f6f27cf760b074a0dc4ffebf8fc6759d46c962f4
Julien <juliencaron18@gmail.com>
2019-08-08 20:32:17 +0200

    Transform blog to shop using snipcart

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,78 +1,88 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
+import Helmet from 'react-helmet'
 
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm, scale } from "../utils/typography"
+import BuyButton from "../components/buyButton"
 
-class BlogPostTemplate extends React.Component {
-  render() {
-    const post = this.props.data.markdownRemark
-    const siteTitle = this.props.data.site.siteMetadata.title
-    const { previous, next } = this.props.pageContext
+export default function BlogPostTemplate ({ data, pageContext, location}) {
+  const post = data.markdownRemark
+  const siteTitle = data.site.siteMetadata.title
+  const { previous, next } = pageContext
+  const images = post.frontmatter.image.map(img => ({
+    name: img.name,
+    src: require(`./../../content/blog${post.frontmatter.path}${img.src}.jpg`)
+  }));
 
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO
-          title={post.frontmatter.title}
-          description={post.frontmatter.description || post.excerpt}
+  return (
+    <Layout location={location} title={siteTitle}>
+      <Helmet htmlAttributes={{ lang: 'en' }}>
+        <title>${siteTitle}</title>
+        <link
+          href="https://cdn.snipcart.com/themes/2.0/base/snipcart.min.css"
+          rel="stylesheet"
+          type="text/css"
         />
-        <h1
-          style={{
-            marginTop: rhythm(1),
-            marginBottom: 0,
-          }}
-        >
-          {post.frontmatter.title}
-        </h1>
-        <p
-          style={{
-            ...scale(-1 / 5),
-            display: `block`,
-            marginBottom: rhythm(1),
-          }}
-        >
-          {post.frontmatter.date}
-        </p>
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
-        <hr
-          style={{
-            marginBottom: rhythm(1),
-          }}
+        <script
+          src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"
         />
-        <Bio />
+        <script
+          id="snipcart"
+          src="https://cdn.snipcart.com/scripts/2.0/snipcart.js"
+          data-api-key="ZDc2MDZlMTMtNzAxMS00NDQ5LWE4NmItZGI4ZTA3ODA2NTRlNjM3MDA4NjM4OTA0MzExMjc3"
+        />
+      </Helmet>
+      <SEO
+        title={post.frontmatter.title}
+        description={post.frontmatter.description || post.excerpt}
+      />
+      <h1 style={{ marginTop: rhythm(1), marginBottom: 0 }}>
+        {post.frontmatter.title}
+      </h1>
+      <p style={{
+        ...scale(-1 / 5),
+        display: `block`,
+        marginBottom: rhythm(1)
+      }}>
+        {post.frontmatter.date}
+      </p>
 
-        <ul
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </ul>
-      </Layout>
-    )
-  }
+      <BuyButton post={post.frontmatter} images={images}/>
+
+      <div dangerouslySetInnerHTML={{ __html: post.html }} />
+
+      <hr style={{ marginBottom: rhythm(1), }}/>
+
+      <Bio />
+
+      <ul style={{
+        display: `flex`,
+        flexWrap: `wrap`,
+        justifyContent: `space-between`,
+        listStyle: `none`,
+        padding: 0
+      }}>
+        <li>
+          {previous && (
+            <Link to={previous.fields.slug} rel="prev">
+              ← {previous.frontmatter.title}
+            </Link>
+          )}
+        </li>
+        <li>
+          {next && (
+            <Link to={next.fields.slug} rel="next">
+              {next.frontmatter.title} →
+            </Link>
+          )}
+        </li>
+      </ul>
+    </Layout>
+  )
 }
-
-export default BlogPostTemplate
 
 export const pageQuery = graphql`
   query BlogPostBySlug($slug: String!) {
@@ -89,7 +99,18 @@ export const pageQuery = graphql`
       frontmatter {
         title
         date(formatString: "MMMM DD, YYYY")
+        price
+        id
+        path
         description
+        image {
+            name
+            src
+        }
+        customField {
+            name
+            values
+        }
       }
     }
   }
